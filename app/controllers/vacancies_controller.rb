8a7c35c6508c286e7bf6c84ecbdee3253957d342
sandor0213 <19950213>
2018-12-06 18:37:59 +0200

    vacanciesr_controller

--- app/controllers/vacancies_controller.rb
+++ app/controllers/vacancies_controller.rb
@@ -1,14 +1,22 @@
 class VacanciesController < ApplicationController
-
-
+before_action :find_vacancy, except: [:index, :new, :create]
+before_action :authenticate_user!, only: [:index, :show]
 
 def index
+	@vacancies = Vacancy.all.order("deadline ASC")
 end
 
 def new
+	@vacancy = Vacancy.new
 end
 
 def create
+	@vacancy = Vacancy.new(vacancy_params)
+	if @vacancy.save
+		redirect_to vacancy_path(@vacancy.id)
+	else
+		render 'new'
+	end
 end
 
 def show
@@ -18,10 +26,26 @@ def edit
 end
 
 def update
+	if @vacancy.update(vacancy_params)
+		redirect_to vacancy_path(@vacancy.id)
+	else
+		render 'edit'
+	end
 end
 
 def destroy
+	@vacancy.destroy
+	redirect_to vacancies_path
 end
 
+private
+
+def find_vacancy
+	@vacancy = Vacancy.find(params[:id])
+end
+
+def vacancy_params
+	params.require(:vacancy).permit(:title, :description, :experience, :deadline)
+end
 
 end
