1989247a2d18df165a31e32a0350b3ca55446b66
sandor0213 <19950213>
2018-12-13 17:36:46 +0200

    DetectMemberlogged?

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 class MembersController < ApplicationController
-
+before_action :find_member, only: [:destroy]
 
 def new
 	@member = Member.new
@@ -8,19 +8,30 @@ end
 def create 
 	@member = Member.new(member_params)
 	if @member.save
-
 		session[:member_id] = @member.id
-
 		redirect_to new_memberparam_path
-		$logged = true
 	else
 		redirect 'new'
 	end
 end
 
+
+def destroy 
+	if @member.destroy
+		redirect_to root_path
+		session[:member_id] = nil
+	end
+end
+
+
+
 private
 def member_params
 	params.require(:member).permit(:username, :password, :passwordconfirmation, :promocode, :firstname, :lastname)
+end
+
+def find_member
+	@member = Member.find(params[:id])
 end
 
 end
